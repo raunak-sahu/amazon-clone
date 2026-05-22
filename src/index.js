9d3320a5d88b0fbe895ddfe67f43bf811ffc120b
raunak-sahu <raunaksahu765@gmail.com>
2026-05-22 19:59:19 +0530

    fix index jsx issue

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
 root.render(
   <React.StrictMode>
 
-    <BrowserRouter>      
+    <BrowserRouter>
+
       <App />
+
     </BrowserRouter>
 
   </React.StrictMode>
 );
-
 
 reportWebVitals();
